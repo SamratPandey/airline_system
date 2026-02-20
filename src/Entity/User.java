3bd6b3872653ff770fd4a05eb1ad4430cd717cd5
gaganyadav2740-crypto <gaganyadav2740@gmail.com>
2026-02-21 00:13:40 +0530

    Update the user entity for few new features and also some others files

--- src/Entity/User.java
+++ src/Entity/User.java
@@ -16,6 +16,17 @@ public class User {
 
     private String password;
 
+    private String role;
 
+    public User() { }
+
+    public User(String firstName, String lastName, String email, String password, String role) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.password = password;
+        this.role = role;
+
+    }
 
 }
